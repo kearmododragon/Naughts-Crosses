f841fcc7f395f8a79ad0888e1be05271b4cc1dfa
Kearney <kearnec@emea.corpdir.net>
2025-07-28 14:55:52 +0200

    TTT completed but for minor bonus features

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const colors = {
 }
 
 const squares = document.querySelectorAll(".square")
-const boardEl = document.querySelector("#board")
-const squareEls = document.querySelectorAll("div.square")
-const winCondition = [
+const statusText = document.querySelector("#statusText");
+const restartButton = document.querySelector("#restartButton");
+// const boardEl = document.querySelector("#board")
+// const squareEls = document.querySelectorAll("div.square")
+const winConditions = [
   [0, 1, 2,],
   [3, 4, 5,],
   [6, 7, 8,],
@@ -16,43 +18,88 @@ const winCondition = [
   [0, 4, 8,],
   [2, 4, 6,],
 ]
-
-const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-let options = ["","","","","","","","","",],
-
-
-
-squareStatus = 0
-let turn = 0
-let currentPlayer = "Px"
-let winner = "none"
+let options = ["", "", "", "", "", "", "", "", "",];
+let currentPlayer = "x"
+let running = false;
 
 InitializeGame();
-function InitializeGame(){
 
+function InitializeGame() {
+  squares.forEach(square => square.addEventListener("click", squareClicked)),
+    restartButton.addEventListener("click", restartGame);
+  statusText.textContent = `${currentPlayer}'s turn`;
+  running = true;
+  console.log("game started")
+};
+function squareClicked() {
+  const squareIndex = this.getAttribute("squareIndex")
+  if (options[squareIndex] != "" || !running) {
+    return;
+  }
+  updateSquare(this, squareIndex);
+  checkWinner();
+  console.log("square clicked ", squareIndex)
+};
+function updateSquare(square, index) {
+  options[index] = currentPlayer;
+  square.textContent = currentPlayer;
+  console.log("updated square")
+};
+function changePlayer() {
+  currentPlayer = (currentPlayer == "x") ? "o" : "x"
+  statusText.textContent = `${currentPlayer}'s turn`
+  console.log('Change Player')
+};
+function checkWinner() {
+  let roundWon = false;
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i];
+    const squareA = options[condition[0]];
+    const squareB = options[condition[1]];
+    const squareC = options[condition[2]];
+    if (squareA == "" || squareB == "" || squareC == "") {
+      continue;
+    }
+    if (squareA == squareB && squareB == squareC) {
+      roundWon = true;
+      break;
+    }
+  }
+  if (roundWon) {
+    statusText.textContent = `${currentPlayer} wins!`;
+    updateScore(currentPlayer);
+    running = false;
+    console.log(statusText);
+  }
+ else if (!options.includes("")) {
+  statusText.textContent = `Tie game! Try again`;
+  running = false
+  console.log(statusText)
 }
-function squareClicked(){
-
-}
-function updateSquare(square, "index"){
-
-}
-function changePlayer(){
-
-}
-function checkWinner(){
-
-}
-function restartGame(){
-
+else {
+  changePlayer()
 }
 
-let pXScore;
-let pOScore;
+console.log("check winner")
+};
+function restartGame() {
+  currentPlayer = "x"
+  options = ["", "", "", "", "", "", "", "", "",];
+  statusText.textContent = `${currentPlayer}'s turn`
+  squares.forEach(square => square.textContent = "")
+  running = true
+  console.log("restart")
+};
 
-// result
+let xScore = 0;
+let oScore = 0;
 
-let pXResult
-let pOResult
-
-// winner condition
+function updateScore(winner) {
+  if (winner === "x") {
+    xScore++;
+    document.getElementById("xScore").textContent = xScore;
+  } else if (winner === "o") {
+    oScore++;
+    document.getElementById("oScore").textContent = oScore;
+  }
+}
